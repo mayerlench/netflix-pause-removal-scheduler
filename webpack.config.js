84a1543d807f187b7ab19f402a04cf60ffaee1a6
Mayer Lench <mayerlench1314@gmail.com>
2020-04-29 23:14:28 -0400

    minor changes

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ const config = {
   mode: process.env.NODE_ENV,
   context: __dirname + "/src",
   entry: {
-    background: "./background.js",
+    "js/background": "./js/background.js",
     "popup/popup": "./popup/popup.js",
     "options/options": "./options/options.js"
   },
